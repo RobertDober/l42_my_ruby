8a5b139667c9d3904c3549fd579477eca4cae08b
Robert Dober <robert.dober@gmail.com>
2022-08-27 08:24:52 +0200

    working on io tool

---
+++ spec/support/input_output_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Support
+  module InputOutputHelper
+    
+  end
+end
+
+RSpec.configure do |config| do
+  config.include Support::InputOutputHelper
+end
+# SPDX-License-Identifier: Apache-2.0
