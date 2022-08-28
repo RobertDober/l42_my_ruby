8363128b3794350c56b8eb38f5064775acd52435
Robert Dober <robert.dober@gmail.com>
2022-08-28 11:11:31 +0200

    v0.1.5

---
+++ lib/l42/interfaces/pfx_by_now.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'securerandom'
+module L42
+  module Interfaces
+    module PfxByNow
+      extend self
+
+      def call(files)
+        now = Time.now.to_i.to_s(16)
+        [
+          :stdout,
+          files.map(&_pfx_by_now(now))
+        ]
+      end
+
+      private
+
+      def _pfx_by_now(now)
+        ->file do
+          ["mv", file.inspect, [now, file].join("_").inspect ].join(" ")
+        end
+      end
+    end
+  end
+end
+# SPDX-License-Identifier: Apache-2.0
